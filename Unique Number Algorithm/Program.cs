3fb54ef30db4b63a73c653abc50c0736bf56f625
Tim-coder-ops <68295710+Tim-coder-ops@users.noreply.github.com>
2020-08-04 20:18:47 -0700

    Initial Commit

---
+++ Unique Number Algorithm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UniqueNumAlgorithm
+{
+    class UniqueNumberAlgorithm
+    {
+        static int FindNum(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+
+                for(int j = 0; j < array.Length; j++)
+                {
+                    if(i != j)
+                    {
+                        if (array[i] == array[j]) count++; 
+                    }
+                }
+
+                if (count == 0) return array[i];
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 1, 2, 3, 3 };
+
+            Console.WriteLine(FindNum(array));
+        }
+    }
+}
